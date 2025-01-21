d6ae78e63909879bd95a67f6ac7654b21e677c78
bangjaeyoung <qkdwo4613@gmail.com>
2025-01-21 22:50:13 +0900

    Lecture 01 - var, val

---
+++ src/main/java/Lec01.java
@@ -0,0 +1,14 @@
+public class Lec01 {
+    
+    public static void main(String[] args) {
+        
+        Long number1 = 10L;
+        final Long number2 = 20L;
+        
+        final String name = "Bang";
+        String name2 = "Jaeyoung";
+        name2 = null;
+        
+        System.out.println(name + name2);
+    }
+}
---
+++ src/main/kotlin/Lec01.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val number1 = 10L
+    val number2: Long = 20L
+
+    val name = "Bang"
+    var name2: String? = "Jaeyoung"
+    name2 = null
+
+    println(name + name2)
+}
